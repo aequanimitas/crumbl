3929216d7bbcf7f39f1793b427b0a00476bcdc6f
aequanimitas <htavenido@gmail.com>
2016-12-14 03:24:04 +0800

    user authentication

--- web/models/user.ex
+++ web/models/user.ex
@@ -16,4 +16,28 @@ defmodule Crumbl.User do
     |> validate_length(:username, min: 1, max: 20)
     |> unique_constraint(:username)
   end
+
+  @doc """
+    Changeset that manager password change
+  """
+  def registration_changeset(model, params) do
+    model
+    # pass to changeset first
+    |> changeset(params)
+    # convert to a changeset for password
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      # if the changeset is valid
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        # add change to changeset, field :password_hash, value from Comeonin
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
 end
