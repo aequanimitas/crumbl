0438919a746a92584f17d99d6ee70ffe48f27b30
aequanimitas <htavenido@gmail.com>
2017-02-20 22:53:34 +0800

    commit before branching out

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,7 +1,13 @@
 defmodule VideoControllerTest do
   use Crumbl.ConnCase
 
-  test "routes that need authentication", %{conn: conn} do
+  setup do
+    user = insert_user(username: "max")
+    conn = assign(build_conn(), :current_user, user)
+    {:ok, conn: conn, user: user}
+  end
+
+  test "routes that need authentication", %{conn: conn, user: user} do
     Enum.each([
       get(conn, video_path(conn, :new)),
       get(conn, video_path(conn, :index)),
@@ -14,5 +20,14 @@ defmodule VideoControllerTest do
       assert html_response(conn, 302)
       assert conn.halted
     end)
+  end
+
+  test "List all user's videos on index", %{conn: conn, user: user} do
+    user_video = insert_video(user, title: "funny cats")
+    other_video = insert_user(username: "hec") |> insert_video(title: "funny cats too")
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
   end
 end
