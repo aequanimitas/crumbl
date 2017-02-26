bff3bfd1dc6d785b1f8d6ce88c444535b7a49867
aequanimitas <htavenido@gmail.com>
2017-02-27 06:28:01 +0800

    adding slugs to video model, migrations

--- web/models/video.ex
+++ web/models/video.ex
@@ -5,6 +5,7 @@ defmodule Crumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Crumbl.User
     belongs_to :category, Crumbl.Category
 
@@ -21,6 +22,21 @@ defmodule Crumbl.Video do
     struct
     |> cast(params, @required_fields, @optional_fields)
     |> validate_required(@required_fields)
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(title) do
+    title
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
