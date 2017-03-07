2350ba082b158d577e680197762fa9d5d39e0cc9
aequanimitas <htavenido@gmail.com>
2017-03-08 02:40:57 +0800

    handling conversations in front-end and back-end

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -3,8 +3,27 @@ defmodule Crumbl.VideoChannel do
 
   # string pattern-matching via concat(??)
   def join("videos:" <> video_id, _params, socket) do
+    # send via erlang
+    :timer.send_interval(5_000, :ping)
     # authorize a "join" attempt, {:error, socket} to done
     # just like plug conn struct, first argument is connection details struct
     {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  end
+
+  def handle_in("new_annotation", params, socket) do
+    # send to ALL users subscribed to the TOPIC
+    # be careful with what you send
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+  end
+
+  # receive here
+  def handle_info(:ping, socket) do
+    count = socket.assigns[:count] || 1
+    push socket, "ping", %{count: count}
+    {:noreply, assign(socket, :count, count + 1)}
   end
 end
--- web/static/js/video.js
+++ web/static/js/video.js
@@ -15,15 +15,47 @@ let Video = {
 	},
 	onReady(videoId, socket) {
 		let msgContainer = document.getElementById("msg-container")
-		let mgsInput = document.getElementById("msg-input")
+		let msgInput = document.getElementById("msg-input")
 		let postButton = document.getElementById("msg-submit")
 		let vidChannel = socket.channel("videos:" + videoId)
 
+		postButton.addEventListener("click", e => {
+			// build payload object
+			let payload = {body: msgInput.value, at: Player.getCurrentTime}
+			// this sync operation makes socket send payload to back-end
+			vidChannel.push("new_annotation", payload)
+				// in case of error
+				.receive("error", e => console.log(e))
+			msgInput.value = ""
+		})
+
+		// catch broadcase message "new_annotation"
+		vidChannel.on("new_annotation", (resp) => {
+			this.renderAnnotation(msgContainer, resp)
+		})	
+
+		vidChannel.on("ping", ({count}) => console.log("PING:", count))
 		vidChannel.join()
 		  // handle both paths (happy and sad)
-			.receive("ok",resp => console.log("accepted: ", resp))
-			.receive("error",resp => console.log("denied: ", resp))
+			.receive("ok", resp => console.log("joined channel ", resp))
+			.receive("error", reason => console.log("join failed: ", resp))
+	},
+	esc(str) {
+		let div = document.createElement("div")
+		div.appendChild(document.createTextNode(str))
+		return div.innerHTML
+	},
+	renderAnnotation(msgContainer, {user, body, at}) {
+		let template = document.createElement("div")
+		template.innerHTML = `
+			<a href="#" data-seek="${this.esc(at)}">
+				<b>${this.esc(user.username)}: </b> ${this.esc(body)}
+			</a>
+		`
+		msgContainer.appendChild(template)
+		msgContainer.scrollTop = msgContainer.scrollHeight
 	}
 }
+
 
 export default Video
